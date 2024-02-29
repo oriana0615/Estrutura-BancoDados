8c5d4e55341449aa6728168192e8f1d654b041b5
oriana <oriana25salazar@gmail.com>
2024-02-28 21:43:22 -0300

    testando javaScript

---
+++ script.js
@@ -0,0 +1,27 @@
+//document.write("Ola mundo")
+
+//const nome = "Regiano alves"
+//const idade = 52
+//const sabeJs = true
+
+//document.write(nome + "\n" + idade + "\n" + sabeJs);
+/*---------------------------------------------------/*
+/*document.write("<ul>");
+document.write("<li>" + nome + "</li>");
+document.write("<li>" + idade + "</li>");
+document.write("<li>" + sabeJs + "</li>");
+document.write("</ul>");
+----------------------------------------------
+/*var num = 10;
+var myName = "Seu Nome";
+var trueValue = true;
+var price = 99.99;
+var nullVar = null;
+var und;
+
+console.log('num: ' + num);
+console.log('meu nome: ' + myName);
+console.log('trueValue: ' + trueValue);
+console.log('price: ' + price);
+console.log('nullVar: ' + nullVar);
+console.log('und: ' + und);/*
